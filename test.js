8cd720187d1f80fa6f94e3750df76e627b2982fe
ashbie <ashbie.b.mweemba@gmail.com>
2023-02-05 00:33:28 +0100

    fix buttons not clicking then buttons showing up on nav-menu in mobile view

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ const navMenu = document.querySelector(".nav-menu");
 const mainNavbar = document.querySelector("#main-navbar");
 const clickOut = document.querySelector(".click-out");
 const footer = document.querySelector("#footer");
+const contact_me = document.querySelector("#contact_me");
 
 // When I click on the hamburger button, activate the classes that have .active appended to them
 hamburger.addEventListener("click", () => {
@@ -11,6 +12,7 @@ hamburger.addEventListener("click", () => {
     mainNavbar.classList.toggle("active");
     clickOut.classList.toggle("active");
     footer.classList.toggle("active");
+    contact_me.classList.toggle("active");
 });
 
 // When I click on any .nav-menu link, make the nav-menu disappear (hide it / default to original css styling)
@@ -20,6 +22,7 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     mainNavbar.classList.remove("active");
     clickOut.classList.remove("active");
     footer.classList.remove("active");
+    contact_me.classList.remove("active");
 }));
 
 // When I click (on the blury part /) away from the .nav-menu, make the .nav-menu disappear and go back to default css styling
@@ -29,4 +32,5 @@ clickOut.addEventListener("click", () => {
     mainNavbar.classList.remove("active");
     clickOut.classList.remove("active");
     footer.classList.remove("active");
+    contact_me.classList.remove("active");
 });
