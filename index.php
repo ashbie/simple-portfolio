95c43720a77c0c497ba1107a7dcc191061ae23d0
Ashbie B. MWEEMBA <ashbie.b.mweemba@gmail.com>
2024-07-26 14:48:23 +0100

    attempt to prevent browsers from caching the CSS file by versioning the CSS file (versioning the CSS file means : appending a version query string to the CSS file link in the HTML file e.g. [ <link rel=stylesheet href=style.css?v=1.1 > ] )

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ echo <<<ZYXABC
       <script src="https://kit.fontawesome.com/64fab6af73.js" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
       <!-- My css -->
-      <link rel="stylesheet" href="test.css">
+      <link rel="stylesheet" href="test.css?v=1.1">
       
   </head>
   <body>
